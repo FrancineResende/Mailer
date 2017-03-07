1e555dbf251559dcd1971bc6b707e52dfb13fc37
Francine Resende <fran.mresende@gmail.com>
2017-03-07 13:41:47 -0300

    "Tentando tirar os arquivos que deveriam ter sido excluídos"

--- web/application/modules/submissions/views/acesso_negado.php
+++
@@ -1,5 +0,0 @@
-<div class="container starter-template">
-<?php
-echo $msg;
-?>
-</div>
--- web/application/modules/users/views/contributor_home.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-<?php echo '<br><h4 style="text-align:center">Olá, ' . $this->session->first_name . ' ' .  $this->session->last_name . '!</h4>';
-
-
-
-?>
-</div>
