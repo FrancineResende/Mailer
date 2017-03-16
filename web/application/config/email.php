16e7cabd652d361b71d12e3632df6032bc93a1d8
Francine Resende <fran.mresende@gmail.com>
2017-03-16 10:20:33 -0300

    Testando

---
+++ web/application/config/email.php
@@ -0,0 +1,14 @@
+<?php
+
+$config['protocol']    = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.gmail.com';
+$config['smtp_port']    = '465';
+$config['smtp_timeout'] = '7';
+$config['smtp_user']    = 'imunodb@gmail.com';
+$config['smtp_pass']    = 'latamimunodb';
+$config['charset']    = 'utf-8';
+$config['newline']    = "\r\n";
+$config['mailtype'] = 'html'; // or text
+$config['validate'] = TRUE; // bool whether to validate email or not
+
+?>
--- web/application/controllers/Mail.php
+++ web/application/controllers/Mail.php
@@ -10,22 +10,25 @@ class Mail extends CI_Controller {
 
 	}
 
-	public function index($nome, $email, $url){
-		$nome  = $this->encryption->decrypt($nome);
-		$email = $this->encryption->decrypt($email);
-		$url   = $this->encryption->decrypt($url);
+	public function index($nome="Fran", $email="Meu_email", $url="url"){
+		echo $nome;
+		echo $email;
+		echo $url;
+		// $nome  = $this->encryption->decrypt($nome);
+		// $email = $this->encryption->decrypt($email);
+		// $url   = $this->encryption->decrypt($url);
 
-		$this->email->from('imunodb@gmail.com', 'ImunoDB');
-        $this->email->reply_to('imunodb@gmail.com', 'ImunoDB');
-        $this->email->$email;
-        $this->email->subject('Confirmação de cadastro - ImunoDB');
-        $this->email->message('Olá, '.$nome.'!'.'<br><br>'.
-                              'Desejamos boas vindas à Base de Dados Latino-Americana de Mutações Genéticas em Imunodeficiências Primárias!'."<br><br>".
-                              'Confirme seu cadastro clicando <a href="'.$url.'"">AQUI</a> ou acessando o link:'."<br><br>".
-                              $url."<br><br>". //gerar link para confirmação do email
-                              'bye bye'.'<br>'); // necessário arrumar texto da mensagem
-                                               // provavelmente mudar para tipo html
-        $this->email->send();
+		// $this->email->from('imunodb@gmail.com', 'ImunoDB');
+  //       $this->email->reply_to('imunodb@gmail.com', 'ImunoDB');
+  //       $this->email->$email;
+  //       $this->email->subject('Confirmação de cadastro - ImunoDB');
+  //       $this->email->message('Olá, '.$nome.'!'.'<br><br>'.
+  //                             'Desejamos boas vindas à Base de Dados Latino-Americana de Mutações Genéticas em Imunodeficiências Primárias!'."<br><br>".
+  //                             'Confirme seu cadastro clicando <a href="'.$url.'"">AQUI</a> ou acessando o link:'."<br><br>".
+  //                             $url."<br><br>". //gerar link para confirmação do email
+  //                             'bye bye'.'<br>'); // necessário arrumar texto da mensagem
+  //                                              // provavelmente mudar para tipo html
+  //       $this->email->send();
 	}
 
 }
