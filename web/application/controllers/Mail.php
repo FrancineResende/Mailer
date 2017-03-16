d65b8190ddd619054ae445b34c20c8df46e4d6a3
Francine Resende <fran.mresende@gmail.com>
2017-03-16 13:50:18 -0300

    Testando

--- web/application/controllers/Mail.php
+++ web/application/controllers/Mail.php
@@ -11,20 +11,25 @@ class Mail extends CI_Controller {
 
 	}
 
-	public function index($nome="", $email="", $url=""){
-		$msg = "";
-		if ($nome=="" or $email=="" or $url==""){
+	public function index($nome='', $id='', $email='', $password='', $datetime=''){
+		$msg = '';
+		if ($nome=='' or $id=='' or $email=='' or $password=='' or $datetime==''){
 			$msg = "ERRO - Ausência de um ou mais parâmetros";
 			// echo "ERRO - Ausência de um ou mais parâmetros";
 			redirect("http://localhost/ImunoDB/users/confirm/".$msg);
 		}
 		else {
-			$nome  = $this->encryption->decrypt($nome);
-			$email = $this->encryption->decrypt($email);
-			echo "url antes = ".$url."\n";
-			$url   = $this->encryption->decrypt($url);
-			$url   = 'http://localhost/codeigniter/mail/confirm?'.$url; 
-			echo "url depois = ".$url."\n";
+			
+			// $id 	  = $this->encryption->decrypt($id);
+			// $password = $this->encryption->decrypt($password);
+			// $datetime = $this->encryption->decrypt($datetime);
+			
+			$url      = 'http://localhost/codeigniter/mail/confirm?';
+			$url	 .= sprintf('id=%s&email=%s&password=%s&datetime=%s',$id, $email, $password, $datetime);
+			
+			$nome     = $this->encryption->decrypt($nome);
+			$email    = $this->encryption->decrypt($email);
+			
 			$this->email->from('imunodb@gmail.com', 'ImunoDB');
 	        $this->email->reply_to('imunodb@gmail.com', 'ImunoDB');
 	        $this->email->to($email);
@@ -36,7 +41,7 @@ class Mail extends CI_Controller {
 	                              'bye bye'.'<br>'); 
 	                              // necessário arrumar texto da mensagem
 	                              // provavelmente mudar para tipo html
-
+	        
 	        echo $nome."\n";
 	        echo $email."\n";
 	        echo $url."\n";
@@ -46,7 +51,7 @@ class Mail extends CI_Controller {
     	    else {
     	    	$msg = "ERRO - Email nao enviado";
     	    	echo "ERRO - Email nao enviado";
-    	    	$this->email->print_debugger();
+    	    	$this->email->print_debugger();	
     	    	// redirect("http://localhost/ImunoDB/users/confirm/".$msg);
     	    }
 		}
