4060426dc7428848ff09fe51c8cf332cc8944858
Francine Resende <fran.mresende@gmail.com>
2017-03-17 12:25:50 -0300

    testando

--- web/application/config/email.php
+++ web/application/config/email.php
@@ -8,7 +8,7 @@ $config['smtp_user']    = 'imunodb@gmail.com';
 $config['smtp_pass']    = 'latamimunodb';
 $config['charset']    = 'utf-8';
 $config['newline']    = "\r\n";
-$config['mailtype'] = 'html'; // or text
+$config['mailtype'] = 'text';//html'; // or text
 $config['validate'] = TRUE; // bool whether to validate email or not
 
 ?>
